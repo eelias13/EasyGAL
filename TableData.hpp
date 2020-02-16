7a60d50a1c9fc2761f4148c4dd57e20fbb5fc67e
Lukas <glukasvomsee@gmail.com>
2020-02-16 20:25:19 +0100

    Removed TableData.cpp and changed TableData class to struct

--- TableData.hpp
+++ TableData.hpp
@@ -4,17 +4,20 @@
 
 using namespace std;
 
-class TableData
+/*
+*   This data structure contains following data from processed expressions.
+*   
+*   - "m_InputPins" stores all the input pins which are used in the expression
+*   - "m_OutputPin" stores the output pin
+*   - "m_Table" contains the truth table for the expression and is used to generate a dnf expression later on
+*   - "m_EnableDFlipFlop" holds a boolean which decides if the output pin should have its flip flop turned on.
+*/
+
+struct TableData 
 {
-
-public:
-
-    bool verify(uint32_t pinMin, uint32_t pinMax, vector<uint32_t> disallowedPins);
-    uint32_t getNumInputBits();
-
-    bool m_enableDFF;
-    uint32_t m_outputPin;
-    vector<bool> m_table;
-    vector<uint32_t> m_inputPins;
+    vector<uint32_t> m_InputPins;
+    uint32_t m_OutputPin;   
+    vector<bool> m_Table;
+    bool m_EnableFlipFlop;
 };
 
