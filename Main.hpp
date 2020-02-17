7dbf469767eb3ec441ea1ae7b1e405a4b21fa9c3
PimpX <46501750+PimpX@users.noreply.github.com>
2020-02-17 23:38:42 +0100

    Moved eeli1's code to Compiler folder

--- Main.hpp
+++ Main.hpp
@@ -1,8 +1,9 @@
 #pragma once
-#include <iostream>
-#include <vector>
 
-#include "Symbols.hpp"
-#include "Helper.hpp"
-#include "Lexer/Pair.hpp"
-#include "Lexer/Lexer.cpp"
+#include <vector>
+#include <iostream>
+
+#include "Compiler/Symbols.hpp"
+#include "Compiler/Helper.hpp"
+#include "Compiler/Lexer/Pair.hpp"
+#include "Compiler/Lexer/Lexer.hpp"
