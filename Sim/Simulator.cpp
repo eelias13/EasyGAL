1bf6d90e23bbe45d39e4fea3118a640d0ca52e42
eeli1 <maierelias13@gmail.com>
2020-07-05 22:52:44 +0200

    updating makefile for multiple executables

---
+++ Sim/Simulator.cpp
@@ -0,0 +1,14 @@
+/*
+ * Simulator.cpp
+ *
+ *  Created on: Jul 5, 2020
+ *      Author: elias
+ */
+
+#include "Simulator.h"
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello world !!" << endl;
+    return 0;
+}
